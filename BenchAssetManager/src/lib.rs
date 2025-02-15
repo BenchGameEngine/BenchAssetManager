d87ef30042299674497102eb726495c51706dcf1
BenchGameEngine <benchgameengine@gmail.com>
2025-02-15 11:40:58 +0000

    Added stuff

--- BenchAssetManager/src/lib.rs
+++ BenchAssetManager/src/lib.rs
@@ -6,16 +6,30 @@ mod BenchAssetManager {
         process::Output,
     };
 
-    fn LoadFile<s: AsRef<str>>(FilePath: s) -> Result<Vec<u8>, String> {
+    struct AssetFileStruct {
+        Data: Vec<u8>,
+    }
+
+    fn ReadFile<s: AsRef<str>>(FilePath: s) -> Result<Vec<u8>, String> {
         fs::read(FilePath.as_ref()).map_err(|e| e.to_string())
+    }
+
+    fn LoadAssetFile<s: AsRef<str>>(AssetPath: s) -> Result<AssetFileStruct, String> {
+        match ReadFile(AssetPath) {
+            Ok(data) => return Ok(AssetFileStruct { Data: data }),
+            Err(error) => return Err(String::from("Error reading file.")),
+        }
     }
 
     #[test]
     fn test_func() {
-        let Output = LoadFile("testfiles/test1");
+        let Output = LoadAssetFile("testfiles/test1");
         match Output {
             Ok(data) => {
                 println!("Loaded fine.");
+                for num in data.Data {
+                    println!("{num}")
+                }
             }
             Err(error) => {
                 println!("Error loading file: {error}");
