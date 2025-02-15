66e219ce1bf99668b79009303ca8bb9817bad55c
BenchGameEngine <benchgameengine@gmail.com>
2025-02-15 11:31:43 +0000

    new testfile and new code.

--- BenchAssetManager/src/lib.rs
+++ BenchAssetManager/src/lib.rs
@@ -1,14 +1,25 @@
 #[allow(non_snake_case)]
 #[cfg(test)]
 mod BenchAssetManager {
-    use std::fs::{self, File};
+    use std::{
+        fs::{self, File},
+        process::Output,
+    };
 
-    fn LoadFile<s: AsRef<str>>(FilePath: s) -> Vec<u8> {
-        return fs::read(FilePath);
+    fn LoadFile<s: AsRef<str>>(FilePath: s) -> Result<Vec<u8>, String> {
+        fs::read(FilePath.as_ref()).map_err(|e| e.to_string())
     }
 
     #[test]
     fn test_func() {
-        let Output = LoadFile("A");
+        let Output = LoadFile("testfiles/test1");
+        match Output {
+            Ok(data) => {
+                println!("Loaded fine.");
+            }
+            Err(error) => {
+                println!("Error loading file: {error}");
+            }
+        }
     }
 }
